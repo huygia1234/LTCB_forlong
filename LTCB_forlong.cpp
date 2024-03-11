6cb0b38b23b5772a05d6603b9d87301dac8af504
ADMIN <ADMIN@LAB1-MAY35>
2024-03-11 09:42:14 +0700

    Add project files.

---
+++ LTCB_forlong.cpp
@@ -0,0 +1,16 @@
+// LTCB_forlong.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int so; cout << "nhap so:"; cin >> so;
+	for (int i = 0; i < so; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
